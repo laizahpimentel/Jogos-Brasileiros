19b24ea1505254e84f4f88dfa6bf49cfb46e23eb
laizahpimentel <laizahpimentel@gmail.com>
2021-09-27 20:29:08 -0300

    acrescentando txt

--- forca.py
+++ forca.py
@@ -4,7 +4,7 @@ def jogar():
     print('***Bem-Vindo no jogo da Forca!***')
     print('*********************************') 
 
-    palavra_secreta = 'banana'.upper()
+    palavra_secreta = 'maça'.upper()
     letras_acertadas = ['_' for letra in palavra_secreta] #ERRO DE CONTINUIDADE
 
     enforcou = False
@@ -26,16 +26,16 @@ def jogar():
                     letras_acertadas[index] = letra
                 index = index + 1 
         else: 
-            erros = erros + 1 
+             erros += 1 
 
         enforcou  = erros == 6 
-        acertou = '-' not in  letras_acertadas 
+        acertou =  '-' not in  letras_acertadas 
         print(letras_acertadas)
 
     if(acertou):
         print('Voce ganhou!!')
     else:
-        print('Voce perdeu!!')
+        print('Voce perdeu!!') 
     print('Fim de Jogo')
     
 
