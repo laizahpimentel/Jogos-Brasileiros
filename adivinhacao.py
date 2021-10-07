978e13e6dc4bd9bf6c86402f4a64ceea462d1a82
ODAIR J.  ROSARIO <84051161+OdairRos@users.noreply.github.com>
2021-10-06 23:52:28 -0300

    Update adivinhacao.py
    Adicionando algumas mudanças e corrigindo erro de sintaxe

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,4 +1,15 @@
-import random
+import random as rand
+
+"""
+alterando a forma como utiliza o print
+de:
+    print('Meu nome é {}'.format(nome))
+para:
+    print(f'Meu nome é {nome}')
+
+~ Odair J.
+"""
+
 
 def jogar():
 
@@ -6,27 +17,29 @@ def jogar():
     print('Bem-Vindo no jogo de Adivinhação!')
     print('*********************************')
 
-    numero_secreto = random.randrange(1,101) 
-    total_de_tentativas = 0
-    pontos = 1000
+    numero_secreto = int(rand.randrange(1,101))
+    total_de_tentativas = int(0)
+    pontos = int(1000)
 
 
-    print('Qual nivel de dificuldade?', numero_secreto)
-    print('(1) Fácil (2) Médio  (3) Difícil')
+    print('Qual nivel de dificuldade?\n (1) Fácil (2) Médio  (3) Difícil', numero_secreto)
 
     nivel = int(input('Define o nível: '))
+
     if(nivel == 1):
         total_de_tentativas = 20 
-    elif(nivel == 2):
+    elif(nivel == 2): 
         total_de_tentativas = 10
     else: 
         total_de_tentativas = 5 
 
     for rodada in range(1, total_de_tentativas + 1):
-        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-        chute_str = input("Digite um número entre 1 e 100: ")
-        print("Você digitou: ", chute_str)
-        chute = int(chute_str)
+        print(f"Tentativa {rodada} de {total_de_tentativas}")
+
+        # convertendo direto para inteiro, ja que não é necessario utilizar str
+        # Não necessario mais  -> chute = int(chute_str)
+        chute = int(input("Digite um número entre 1 e 100: "))
+        print(f"Você digitou: {chute}")
 
         if(chute < 1 or chute > 100):
             print('Voce deve digitar um número entre 1 e 100!')
@@ -37,18 +50,19 @@ def jogar():
         menor = chute < numero_secreto
 
         if(acertou):
-            print('Voce acertou e fez {] pontos!'.format(pontos))
+            print(f'Voce acertou e fez {pontos} pontos!')
+            # Possuia um erro nesta  linha 
+            # print('Voce acertou e fez {] pontos!'.format(pontos))
             break
         else:
             if(maior):
                 print('Voce errou! O seu chute foi maior do que o número secreto.')
             elif(menor):
                 print('Voce errou! O seu chute foi menor do que o número secreto.')
-            pontos_perdidos = abs(numero_secreto - chute) 
-            pontos = pontos - pontos_perdidos
+            # Para poupar memoria esta variavel não é necessaria
+            #pontos_perdidos = abs(numero_secreto - chute) 
+            pontos = pontos - abs(numero_secreto - chute) 
 
             print('Fim de Jogo')
-if (__name__=='__main__'):
+if (__name__ == '__main__'):
     jogar()
-
-
